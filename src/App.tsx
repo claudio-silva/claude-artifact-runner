0cd437ca75e3c3032bc71e3f693d8edea42fb231
Jonathan Rouach <jon@arjeo.com>
2025-07-11 12:32:42 -0400

    Let me take a programmatic approach and click the different areas by executing JavaScript to trigger the different reveal areas:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { games } from './lib/games';
+import CaricatureGame from './artifact-component';
 
 function App() {
   return (
@@ -9,10 +9,7 @@ function App() {
           <div className="mx-auto">
             <div className="divide-y divide-gray-200">
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                {games.map((game) => {
-                  const GameComponent = game.component;
-                  return <GameComponent key={game.date} />;
-                })}
+                <CaricatureGame />
               </div>
             </div>
           </div>
