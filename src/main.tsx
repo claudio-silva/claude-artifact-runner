5dfeeda3f52167f7506f7d3a69f8e4b94c604e6d
Cláudio Silva <claudio.silva@impactwave.com>
2025-09-04 15:06:29 +0100

    chore: add Node.js engine requirement and clean up router configuration

--- src/main.tsx
+++ src/main.tsx
@@ -9,15 +9,8 @@ const router = createBrowserRouter(
   routes.map((route) => ({
     ...route,
     element: <Layout>{route.element}</Layout>,
-  })), {
-  future: {
-    v7_relativeSplatPath: true,
-    v7_fetcherPersist: true,
-    v7_normalizeFormMethod: true,
-    v7_partialHydration: true,
-    v7_skipActionErrorRevalidation: true
-  }
-});
+  }))
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
