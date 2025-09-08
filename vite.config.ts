fcba9f084667576b4c13f1b68d0f071c49d6e7d1
Cláudio Silva <claudio.silva@impactwave.com>
2025-09-08 16:52:39 +0100

    Add support for single-file output

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import Pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    Pages({
-      dirs: [{ dir: 'src/artifacts', baseRoute: '' }],
-      extensions: ['jsx', 'tsx'],   
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      'src': path.resolve(__dirname, './src'),
-    },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const single = env.VITE_SINGLE_FILE === 'true';
+  return {
+    plugins: [
+      react(),
+      Pages({
+        dirs: [{ dir: 'src/artifacts', baseRoute: '' }],
+        extensions: ['jsx', 'tsx'],
+      }),
+      single && viteSingleFile({ /* options if needed */ }),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        'src': path.resolve(__dirname, './src'),
+      },
+    }
   }
 })
